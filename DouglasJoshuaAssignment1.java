e00adb64038ae3aae2d91eed2e5599335611671f
J <123434357+Dread63@users.noreply.github.com>
2024-01-21 12:05:12 -0700

    Sprint 2
    Completed first 2 objectives of assignment. Data sorting still required.

--- DouglasJoshuaAssignment1.java
+++ DouglasJoshuaAssignment1.java
@@ -1,4 +1,5 @@
-
+import java.util.*;
+import java.io.*;
 /*
  * Name: Joshua Douglas
  * Class: CS 1450 - 001 (Tue/Thu)
@@ -14,11 +15,25 @@ public class DouglasJoshuaAssignment1 {
         int size1 = 1 + (int)(Math.random() * 10);
         int size2 = 1 + (int)(Math.random() * 10);
 
-        System.out.println("Size 1 is " + size1);
-        System.out.println("Size 2 is " + size2);
+        System.out.println("The length of array 1 is " + size1);
+        System.out.println("The length of array 2 is " + size2);
 
         int[] size1Array = generateNumbers(size1);
         int[] size2Array = generateNumbers(size2);
+        Arrays.sort(size1Array);
+        Arrays.sort(size2Array);
+        printArrayData(size1Array);
+        printArrayData(size2Array);
+
+        
+        File arrayData = new File("assignment1data.txt");
+        PrintWriter resultsFile = new PrintWriter(arrayData);
+
+        for (int i=0; i < size1Array.length; i++) {
+
+            resultsFile.println(size1Array[i]);
+        }
+
     }
 
     public static int[] generateNumbers(int size) {
@@ -34,5 +49,11 @@ public class DouglasJoshuaAssignment1 {
 
         return randomNumArray;
     }
-     
+
+    public static void printArrayData(int[] array) {
+
+        for (int i=0; i<array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }    
 }
