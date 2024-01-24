6bec4013fdd0683c7959ac9fcd46d99699dcac33
J <123434357+Dread63@users.noreply.github.com>
2024-01-24 13:20:37 -0700

    Sprint 3 Complete
    Program fully functioning as assigned. Code cleanup and documentation required.

--- DouglasJoshuaAssignment1.java
+++ DouglasJoshuaAssignment1.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.io.File;
 /*
  * Name: Joshua Douglas
  * Class: CS 1450 - 001 (Tue/Thu)
@@ -34,25 +35,17 @@ public class DouglasJoshuaAssignment1 {
 
         System.out.println("Beggining file writing");
 
-        int shortestArray;
-
-        if (size1Array.length >= size2Array.length) {
-            shortestArray = size2Array.length;
-        }
-        else {
-            shortestArray = size1Array.length;
-        }
-
-        int x = 0;
-        int z = 0;
-
-        for (int i = 0; i < shortestArray; i++) {
-            
-
-            
-        }
+        writeDataToFile(size1Array, size2Array, resultingData);
         resultingData.close();
         System.out.println("DONE");
+
+        Scanner scanner = new Scanner(arrayData);
+
+        int[] noDuplicates = removeDuplicates(size1Array, size2Array, scanner);
+        System.out.println("Printing array with no duplicates");
+        for (int i = 0; i < noDuplicates.length; i++) {
+            System.out.println(noDuplicates[i]);
+        }
     }
 
     public static int[] generateNumbers(int size) {
@@ -126,7 +119,60 @@ public class DouglasJoshuaAssignment1 {
         }
 
         while (i < array1.length) {
-
+            System.out.println("Writing " + array1[i]);
+            writer.println(array1[i]);
+            i++;
         }
+
+        while (z < array2.length) {
+            System.out.println("Writing " + array2[z]);
+            writer.println(array2[z]);
+            z++;
+        }
+
+        writer.close();
+    }
+
+    public static int[] removeDuplicates(int[] array1, int[] array2, Scanner scanner) throws IOException {
+
+        int previousLine = 0;
+        int[] noDuplicates = new int[array1.length + array2.length];
+        int i = 0;
+
+        do {
+
+            int currentLine = scanner.nextInt();
+
+            if (currentLine != previousLine) {
+
+                noDuplicates[i] = currentLine;
+                previousLine = currentLine;
+                i++;
+            }
+
+        } while (scanner.hasNextInt());
+
+        scanner.close();
+
+        int zeroCounter = 0;
+        for (int z = 0; z < noDuplicates.length; z++) {
+
+            if (noDuplicates[z] == 0) {
+                zeroCounter++;
+            }
+        }
+
+        int[] noDuplicatesNoZeros = new int[noDuplicates.length - zeroCounter];
+
+        int index = 0;
+        for (int z = 0; z < noDuplicatesNoZeros.length; z++) {
+
+            if (noDuplicates[z] != 0) {
+                noDuplicatesNoZeros[index] = noDuplicates[z];
+                index++;
+            }
+        }
+
+        return noDuplicatesNoZeros;
     }
 }
