993cce8544aaf10d6defc43279e31cda9ed345ac
J <123434357+Dread63@users.noreply.github.com>
2024-01-29 10:40:19 -0700

    Sprint 2 Complete
    Program functions as assigned. Code cleanup awaiting.

--- DouglasJoshuaAssignment2.java
+++ DouglasJoshuaAssignment2.java
@@ -22,146 +22,173 @@ public class DouglasJoshuaAssignment2 {
             String type = scanner.next();
             String brand = scanner.next() + " " + scanner.nextLine();
 
-            if (type.equals("Snowboard")) {
+            if (type.equals("snowboard")) {
 
                 skis[index] = new Snowboard(brand);
+                index++;
+            }
+
+            if (type.equals("cross-country")) {
+
+                skis[index] = new CrossCountry(brand);
+                index++;
+            }
+
+            if (type.equals("downhill")) {
+
+                skis[index] = new Downhill(brand);
+                index++;
             }
         }
+
+        System.out.println("------------------------------------------------------------------------------------------------------------");
+        System.out.printf("%-15s%-25s%-10s\n", "Type", "Brand", "Description");
+        System.out.println("------------------------------------------------------------------------------------------------------------");
+
+        for (int i = 0; i < skis.length; i++) {
+
+            System.out.printf("%-15s%-25s%-15s\n", skis[i].getType(), skis[i].getBrand(), skis[i].description());
+        }
+
+        System.out.println();
+
+        SkiShop skiShop = new SkiShop();
+        skiShop.fillSkiShop(skis);
+        skiShop.printSkiShopDetails();
+    }
+}
+
+class Ski {
+
+    private String type;
+    private String brand;
+
+    public Ski(String type, String brand) {
+        this.type = type;
+        this.brand = brand;
     }
 
-    class Ski {
+    public String getType() {
+        return type;
+    }
 
-        private String type;
-        private String brand;
+    public String getBrand() {
+        return brand;
+    }
 
-        public Ski(String type, String brand) {
-            this.type = type;
-            this.brand = brand;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public String getBrand() {
-            return brand;
-        }
-
-        public String description() {
+    public String description() {
             return type + " " + brand; // CHECK THIS
-        }
+    }
+}
+
+class CrossCountry extends Ski {
+
+    public CrossCountry(String brand) {
+
+        super("Cross Country", brand);
     }
 
-    class CrossCountry extends Ski {
+    @Override
+    public String description() {
+        return "Long & thin skis with binding that allows heal to be free";
+    }
+}
 
-        public CrossCountry(String brand) {
+class Downhill extends Ski {
 
-            super("Cross Country", brand);
-        }
+    public Downhill(String brand) {
 
-        @Override
-        public String description() {
-            return "Long & thin skis with binding that allows heal to be free";
-        }
+        super("Downhill", brand);
     }
 
-    class Downhill extends Ski {
-
-        public Downhill(String brand) {
-
-            super("Downhill", brand);
-        }
-
-        @Override
-        public String description() {
-            return "All-mountain skis where width depends on skill level";
-        }
+    @Override
+    public String description() {
+        return "All-mountain skis where width depends on skill level";
     }
+}
 
-    class Snowboard extends Ski {
+class Snowboard extends Ski {
 
-        public Snowboard(String brand) {
+    public Snowboard(String brand) {
             
-            super("Snowboard", brand);
-        }
-
-        @Override
-        public String description() {
-            return "Single board where both feet are secured onto board in two bindings";
-        }
+        super("Snowboard", brand);
     }
 
-    class SkiShop {
+    @Override
+    public String description() {
+        return "Single board where both feet are secured onto board in two bindings";
+    }
+}
 
-        private int numDownhill;
-        private int numCrossCountry;
-        private Ski[] skisForSale;
+class SkiShop {
 
-        public void fillSkiShop (Ski[] skis) {
+    private int numDownhill;
+    private int numCrossCountry;
+    private Ski[] skisForSale;
 
-            int index = 0;
-            numDownhill = 0;
-            numCrossCountry = 0;
+    public void fillSkiShop(Ski[] skis) {
+
+        int index = 0;
+        numDownhill = 0;
+        numCrossCountry = 0;
             
-            for (int i = 0; i < skis.length; i++) {
+        for (int i = 0; i < skis.length; i++) {
 
-                if (skis[i] instanceof Downhill) {
+            if (skis[i] instanceof Downhill) {
                     
-                    numDownhill++;
-                }
-
-                if (skis[i] instanceof CrossCountry) {
-                    
-                    numCrossCountry++;
-                }
+                numDownhill++;
             }
 
-            skisForSale = new Ski[numCrossCountry + numDownhill];
-
-            for (int i = 0; i < skis.length; i++) {
-
-                if (skis[i] instanceof Downhill || skis[i] instanceof CrossCountry) {
-                    skisForSale[index] = skis[i];
-                    index++;
-                }
+            if (skis[i] instanceof CrossCountry) {
+                    
+                numCrossCountry++;
             }
         }
 
-        public void printSkiShopDetails() {
+        skisForSale = new Ski[numCrossCountry + numDownhill];
 
-            int numDownhill = 0;
-            int numCrossCountry = 0;
+        for (int i = 0; i < skis.length; i++) {
 
-            for (int i = 0; i < skisForSale.length; i++) {
-
-                if (skisForSale[i] instanceof Downhill) {
-                    numDownhill++;
-                }
-
-                if (skisForSale[i] instanceof CrossCountry) {
-                    numCrossCountry++;
-                }  
-            }
-
-            System.out.println("The ski shop currently has " + numDownhill + " downhill skis in stock");
-            System.out.println("The ski shop currently has " + numCrossCountry + " cross country skis in stock");
-            System.out.println("------------------------------Ski Stock---------------------------");
-
-            for (int i = 0; i < skisForSale.length; i++) {
-
-                if (skisForSale[i] instanceof Downhill) {
-                    System.out.println(skisForSale[i].getType() + skisForSale[i].getBrand());
-                }
-            }
-
-            for (int i = 0; i < skisForSale.length; i++) {
-
-                if (skisForSale[i] instanceof CrossCountry) {
-                    System.out.println(skisForSale[i].getType() + skisForSale[i].getBrand());
-                }
+            if (skis[i] instanceof Downhill || skis[i] instanceof CrossCountry) {
+                skisForSale[index] = skis[i];
+                index++;
             }
         }
     }
 
+    public void printSkiShopDetails() {
 
+        int numDownhill = 0;
+        int numCrossCountry = 0;
+
+        for (int i = 0; i < skisForSale.length; i++) {
+
+            if (skisForSale[i] instanceof Downhill) {
+                numDownhill++;
+            }
+
+            if (skisForSale[i] instanceof CrossCountry) {
+                numCrossCountry++;
+            }  
+        }
+
+        System.out.println("----------------------------Ski Shop---------------------------");
+        System.out.println("Downhill skis available: " + numDownhill);
+        System.out.println("Cross-country skis available: " + numCrossCountry);
+        
+
+        for (int i = 0; i < skisForSale.length; i++) {
+
+            if (skisForSale[i] instanceof Downhill) {
+                System.out.println(skisForSale[i].getType() + " --- " + skisForSale[i].getBrand());
+            }
+        }
+
+        for (int i = 0; i < skisForSale.length; i++) {
+
+            if (skisForSale[i] instanceof CrossCountry) {
+                System.out.println(skisForSale[i].getType() + " --- " + skisForSale[i].getBrand());
+            }
+        }
+    }
 }
