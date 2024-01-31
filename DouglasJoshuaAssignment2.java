1f7ea50931752573af7ccfb3c35de2b4c98f20c8
J <123434357+Dread63@users.noreply.github.com>
2024-01-31 11:42:14 -0700

    Sprint 3 Complete
    Assignment polished and ready to be uploaded

--- DouglasJoshuaAssignment2.java
+++ DouglasJoshuaAssignment2.java
@@ -5,18 +5,22 @@ import java.io.*;
  * Class: CS 1450 - 001 (Tue/Thu)
  * Date: 02.01.24
  * Assignment #2
- * Description: 
+ * Description: This program is designed to review the manipulation of objects, inheritance, and polymorphism. The code creates a ski parent class with subclasses Cross-Country,
+ * Downhill, and Snowboard. Then, these skis will be place into a polymorphic array and storted into the "ski shop" depending on their type. Lastly, the code will print the 
+ * details of the ski shop in a nicely formatted table for the end user.
  */
 
 public class DouglasJoshuaAssignment2 {
+
     public static void main(String[] args) throws IOException {
         
+        // Declaring neccisary variables
         File skisTxt = new File("Skis.txt");
         Scanner scanner = new Scanner(skisTxt);
-
         Ski[] skis = new Ski[scanner.nextInt()];
         int index = 0;
 
+        // Reading from the skis.txt file until hitting a blank line
         while (scanner.hasNextLine()) {
 
             String type = scanner.next();
@@ -41,6 +45,7 @@ public class DouglasJoshuaAssignment2 {
             }
         }
 
+        // Printing ski type, brand, and description using a for loop
         System.out.println("------------------------------------------------------------------------------------------------------------");
         System.out.printf("%-15s%-25s%-10s\n", "Type", "Brand", "Description");
         System.out.println("------------------------------------------------------------------------------------------------------------");
@@ -52,12 +57,14 @@ public class DouglasJoshuaAssignment2 {
 
         System.out.println();
 
+        // Passing data to skiShop to fill and print for the user
         SkiShop skiShop = new SkiShop();
         skiShop.fillSkiShop(skis);
         skiShop.printSkiShopDetails();
     }
 }
 
+// Ski parent class with constructor and getters
 class Ski {
 
     private String type;
@@ -76,16 +83,18 @@ class Ski {
         return brand;
     }
 
+    // This method will be overriden by subclasses
     public String description() {
-            return type + " " + brand; // CHECK THIS
+        return type + " " + brand;
     }
 }
 
+// Child class of ski with unique constructor and description
 class CrossCountry extends Ski {
 
     public CrossCountry(String brand) {
 
-        super("Cross Country", brand);
+        super("Cross Country", brand); // Call to parent "ski" constructor
     }
 
     @Override
@@ -94,11 +103,12 @@ class CrossCountry extends Ski {
     }
 }
 
+// Child class of ski with unique constructor and description
 class Downhill extends Ski {
 
     public Downhill(String brand) {
 
-        super("Downhill", brand);
+        super("Downhill", brand); // Call to parent "ski" constructor
     }
 
     @Override
@@ -107,11 +117,12 @@ class Downhill extends Ski {
     }
 }
 
+// Child class of ski with unique constructor and description
 class Snowboard extends Ski {
 
     public Snowboard(String brand) {
             
-        super("Snowboard", brand);
+        super("Snowboard", brand); // Call to parent "ski" constructor
     }
 
     @Override
@@ -120,8 +131,10 @@ class Snowboard extends Ski {
     }
 }
 
+// Class which will be filled with only downhill and cross country skis
 class SkiShop {
 
+    // Ints to county skis, array where those skis will be stored
     private int numDownhill;
     private int numCrossCountry;
     private Ski[] skisForSale;
@@ -131,7 +144,8 @@ class SkiShop {
         int index = 0;
         numDownhill = 0;
         numCrossCountry = 0;
-            
+         
+        // For loop to determine the proper length of the skisForSale array
         for (int i = 0; i < skis.length; i++) {
 
             if (skis[i] instanceof Downhill) {
@@ -145,8 +159,9 @@ class SkiShop {
             }
         }
 
-        skisForSale = new Ski[numCrossCountry + numDownhill];
+        skisForSale = new Ski[numCrossCountry + numDownhill]; // Length of array initialized
 
+        // For loop to fill skisForSale with only the downhill and crosscountry skis
         for (int i = 0; i < skis.length; i++) {
 
             if (skis[i] instanceof Downhill || skis[i] instanceof CrossCountry) {
@@ -156,11 +171,13 @@ class SkiShop {
         }
     }
 
+    // Print what skis are for sale
     public void printSkiShopDetails() {
 
         int numDownhill = 0;
         int numCrossCountry = 0;
 
+        // Tell customer how many of each are available
         for (int i = 0; i < skisForSale.length; i++) {
 
             if (skisForSale[i] instanceof Downhill) {
@@ -176,7 +193,7 @@ class SkiShop {
         System.out.println("Downhill skis available: " + numDownhill);
         System.out.println("Cross-country skis available: " + numCrossCountry);
         
-
+        // Print the type, brand, and description of each ski
         for (int i = 0; i < skisForSale.length; i++) {
 
             if (skisForSale[i] instanceof Downhill) {
