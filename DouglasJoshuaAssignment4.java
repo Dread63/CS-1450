d038b8bb894c37130b7b403d11d97cc5d034f722
Josh Douglas <jmdcolorado11@protonmail.com>
2024-02-22 13:35:21 -0700

    Sprint 2 Complete

--- DouglasJoshuaAssignment4.java
+++ DouglasJoshuaAssignment4.java
@@ -1,9 +1,63 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class DouglasJoshuaAssignment4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        File pinballMachineTargets = new File("PinballMachineTargets.txt");
+        File play = new File ("Play.txt");
+        Scanner targetScanner = new Scanner(pinballMachineTargets);
+
+        int numRows = targetScanner.nextInt();
+        int numColumns = targetScanner.nextInt();
+
+        PinballMachine pinballMachine = new PinballMachine(numRows, numColumns);
+
+        while (targetScanner.hasNextLine()) {
+
+            int row = targetScanner.nextInt();
+            int column = targetScanner.nextInt();
+            String targetType = targetScanner.next();
+            int id = targetScanner.nextInt();
+            int points = targetScanner.nextInt();
+
+            Target target = new Target(targetType, id, points);
+
+            pinballMachine.addTargetToPlayingField(row, column, target);
+        }
+        
+        pinballMachine.displayPlayingField();;
+
+        pinballMachine.play(play);
+
+        printTargetsByHits(pinballMachine);
+
     }
+
+    public static void printTargetsByHits(PinballMachine pinballMachine) {
+
+        ArrayList<Target> targetsArrayList = new ArrayList<>();
+
+        for (int i = 0; i < pinballMachine.getNumRows(); i++) {
+
+            for (int j = 0; j < pinballMachine.getNumColumns(); j++) {
+
+                Target currentTarget = pinballMachine.getTarget(i, j);
+
+                if (currentTarget != null) {
+                    targetsArrayList.add(currentTarget);
+                }  
+            }
+        }
+
+        // Sorting arraylist using the overridden method compareTo in the Target class
+        Collections.sort(targetsArrayList);
+
+        for (int i = 0; i < targetsArrayList.size(); i++) {
+            System.out.println(targetsArrayList.get(i).toString());
+        }
+    }
+
 }
 
 class PinballMachine {
@@ -51,15 +105,44 @@ class PinballMachine {
             System.out.println();
             System.out.printf("%s%5s", "Row", i);
 
-            for (int j = 0; j < numColumns; i++) {
+            for (int j = 0; j < numColumns; j++) {
 
                 if (playingField[i][j] == null) {
                     System.out.printf("%5s", "-----");
                 }
 
                 else {
-                    System.out.printf("%5s", playingField[i][j].get);
+                    System.out.printf("%5s", playingField[i][j]);
                 }
+            }
+        }
+    }
+
+    public void play(File playFileName) throws IOException {
+
+        Scanner playScanner = new Scanner(playFileName);
+
+        int score = 0;
+
+        System.out.println("---------------------");
+        System.out.println("Simulate Pinball Game");
+        System.out.println("---------------------");
+
+        System.out.printf("%-10s%-10s%-10s%-10s", "Target hit", "ID", "Points", "Score");
+        System.out.println("---------------------");
+
+
+        while (playScanner.hasNextLine()) {
+
+            int row = playScanner.nextInt();
+            int column = playScanner.nextInt();
+
+            if (playingField[row][column] != null) {
+
+                playingField[row][column].incrementHits();
+                score += playingField[row][column].getPoints();
+                System.out.printf("%-10s%-10d%-10d%-10d", playingField[row][column].getType(), playingField[row][column]
+                .getId(), playingField[row][column].getPoints(), score);
             }
         }
     }
