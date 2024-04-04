92a4be887bc51a23b944ca0b675b1f2ce362559d
J <123434357+Dread63@users.noreply.github.com>
2024-04-04 10:37:15 -0600

    Finalized Assignment 8

--- DouglasJoshuaAssignment8.java
+++ DouglasJoshuaAssignment8.java
@@ -4,39 +4,73 @@ import java.io.*;
 public class DouglasJoshuaAssignment8 {
     public static void main(String[] args) throws IOException {
         
+        // Opening Files for Reading
         File queueMessage = new File("queueMessage.txt");
         File listMessage = new File("listMessage.txt");
         File queueKey = new File("queueKey.txt");
         File listKey = new File("listKey.txt");
+
         Scanner queueMsgScanner = new Scanner(queueMessage);
         Scanner listMsgScanner = new Scanner(listMessage);
         Scanner queueKeyScanner = new Scanner(queueKey);
         Scanner listKeyScanner = new Scanner(listKey);
 
-
+        // Collecting messages and keys from scanners
         String listMessageString = listMsgScanner.nextLine();
         String listKeysString = listKeyScanner.nextLine();
+        String queueMessageString = queueMsgScanner.nextLine();
+        String queueKeyString = queueKeyScanner.nextLine();
 
-        // Gettings dimensions for 2D array from listKeys.txt
+        // Getting dimensions for 2D array from listKeys.txt, and queueKeys.txt
         int listRowSize = listKeyScanner.nextInt();
         int listColSize = listKeyScanner.nextInt();
-        int routeStartingRow = listKeyScanner.nextInt();
-        int routeStartingCol = listKeyScanner.nextInt();
+        int listRouteStartingRow = listKeyScanner.nextInt();
+        int listRouteStartingCol = listKeyScanner.nextInt();
 
-        // Filling arrays with values from text files
+        int queueRowSize = queueKeyScanner.nextInt();
+        int queueColSize = queueKeyScanner.nextInt();
+        int queueRouteStartingRow = queueKeyScanner.nextInt();
+        int queueRouteStartingCol = queueKeyScanner.nextInt();
+
+        // Filling arrays and queues with values from text files
         ArrayList<Character> listMessageArray = stringToCharArray(listMessageString);
         ArrayList<Character> listKeysArray = stringToCharArray(listKeysString);
+
+        Queue<Character> queueMessageQueue = stringToCharQueue(queueMessageString);
+        Queue<Character> queueKeysQueue = stringToCharQueue(queueKeyString);
         
+        // Interators used to push values onto stack in unsramble method
         Iterator<Character> listMessageIterator = listMessageArray.iterator();
         Iterator<Character> listKeysIterator = listKeysArray.iterator();
+        Iterator<Character> queueMessageIterator = queueMessageQueue.iterator();
+        Iterator<Character> queueKeysIterator = queueKeysQueue.iterator();
 
-        Decoder decoder = new Decoder(listRowSize, listColSize, routeStartingRow, routeStartingCol);
+        // Enables us to use unscramble method inside Decoder class
+        Decoder listDecoder = new Decoder(listRowSize, listColSize, listRouteStartingRow, listRouteStartingCol);
+        Decoder queueDecoder = new Decoder(queueRowSize, queueColSize, queueRouteStartingRow, queueRouteStartingCol);
 
-        String finalMessage = decoder.unscramble(listMessageIterator, listKeysIterator);
+        // Storing final secret messages
+        String finalListMessage = listDecoder.unscramble(listMessageIterator, listKeysIterator);
+        String finalQueueMessage = queueDecoder.unscramble(queueMessageIterator, queueKeysIterator);
 
-        System.out.println(finalMessage);
+        // Display secret messages to user
+        System.out.print("---------------");
+        System.out.printf("%-15s", "LIST SECRET MESSAGE");
+        System.out.println("---------------");
+        System.out.println(finalListMessage);
+        System.out.print("---------------");
+        System.out.printf("%-15s", "QUEUE SECRET MESSAGE");
+        System.out.println("---------------");
+        System.out.println(finalQueueMessage);
+
+        // Best Practice: Closing Scanners to Avoid Memory Leaks
+        queueMsgScanner.close();
+        listMsgScanner.close();
+        queueKeyScanner.close();
+        listKeyScanner.close();
     }
 
+    // Breaks string into characters and place into Character arrayList
     public static ArrayList<Character> stringToCharArray(String string) {
 
         ArrayList<Character> charArray = new ArrayList<>();
@@ -48,8 +82,22 @@ public class DouglasJoshuaAssignment8 {
 
         return charArray;
     }
+
+    // Breaks string into characters and places into Character queue
+    public static Queue<Character> stringToCharQueue(String string) {
+        
+        Queue<Character> charQueue = new LinkedList<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            Character currentChar = string.charAt(i);
+            charQueue.add(currentChar);
+        }
+
+        return charQueue;
+    }
 }
 
+// Class which contains 2D array and unscramble method to decode our secret messages
 class Decoder {
 
     private char[][] messageArray;
@@ -67,6 +115,7 @@ class Decoder {
         this.startingCol = startingCol;
     }
 
+    // Unscrambles secret message using iterators
     public String unscramble (Iterator<Character> msgIterator, Iterator<Character> keyIterator) {
 
         int row = startingRow;
@@ -76,6 +125,7 @@ class Decoder {
         int numRows = messageArray.length;
         int numColumns = messageArray[0].length;
 
+        // Add message to 2D message array
         for (int i = numRows - 1; i >= 0; i--) {
 
             for (int z = 0; z < numColumns; z++) {
@@ -84,8 +134,10 @@ class Decoder {
             }
         }
 
+        // Push the first value onto the defined starting positions
         stack.push(messageArray[row][col]); 
         
+        // Iterate through keys, move through 2D array and push values onto stack
         while (keyIterator.hasNext()) {
             
             char direction = keyIterator.next();
@@ -109,8 +161,9 @@ class Decoder {
             stack.push(messageArray[row][col]);
         }
 
+        // Assemble final message from characters on stack
         int size = stack.size();
-        String finalMessage;
+        String finalMessage = "";
 
         for (int i = 0; i < size; i++) {
 
@@ -118,12 +171,12 @@ class Decoder {
             finalMessage = finalMessage + currentChar;
         }
 
-
         return finalMessage;
     }
 
 }
 
+// Custom stack to ensure proper stack behavior
 class DecodingStack {
 
     private Stack<Character> JCFstack;
