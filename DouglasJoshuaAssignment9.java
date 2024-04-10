64ab7a4c80287f3fa5698b2d0bc5dd27f8c5bf60
J <123434357+Dread63@users.noreply.github.com>
2024-04-09 18:36:45 -0600

    Sprint 1 Complete

---
+++ DouglasJoshuaAssignment9.java
@@ -0,0 +1,171 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DouglasJoshuaAssignment9 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        File itinerary = new File("ParisItinerary.txt");
+        Scanner fileReader = new Scanner(itinerary);
+
+        ItineraryLinkedList singleLinkedList = new ItineraryLinkedList();
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+
+        while (fileReader.hasNext()) {
+
+            String line = fileReader.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            int stop = lineScanner.nextInt();
+            String type = lineScanner.next();
+            String name = lineScanner.next();
+            String activity = lineScanner.nextLine();
+
+            Destination newDestination = new Destination(stop, type, name, activity);
+
+            singleLinkedList.addByStopNumber(newDestination);
+            lineScanner.close();
+        }
+        fileReader.close();
+
+        singleLinkedList.printList();
+    }
+}
+
+// Represents one destination (stop) in the itinerary
+class Destination {
+
+    private int stop;
+    private String type;
+    private String name;
+    private String activity;
+
+    public Destination (int stop, String type, String name, String activity) {
+
+        this.stop = stop;
+        this.type = type;
+        this.name = name;
+        this.activity = activity;  
+    }
+
+    public int getStop() {
+        return stop;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setStop(int stop) {
+        this.stop = stop;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String toString() {
+        
+        return String.format("%-2d%-25s%-20s%s", stop, name, type, activity);
+    }
+}
+
+class ItineraryLinkedList {
+
+    private Node head;
+
+    public void addByStopNumber(Destination destinationToAdd) {
+
+        Node current = head;
+        Node tempNode = new Node(destinationToAdd);
+
+        if (head == null || destinationToAdd.getStop() < head.destination.getStop()) {
+
+            tempNode.next = head;
+            head = tempNode;
+        }
+
+        else {
+
+            while (current.next != null && destinationToAdd.getStop() > current.next.destination.getStop()) {
+
+                current = current.next;
+            }
+
+            tempNode.next = current.next;
+            current.next = tempNode;
+        }         
+
+    }
+
+    public Destination removeDestination(String typeToRemove) {
+
+    }
+
+    void reverse() {
+
+    }
+
+    public void printList() {
+
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.destination);
+            current = current.next;
+        }
+    }
+
+    private static class Node {
+
+        private Destination destination;
+        private Node next;
+
+        public Node(Destination destination) {
+            this.destination = destination;
+            next = null;
+        }
+    }
+}
+
+class DoubleLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    public void addDestination (Destination destination) {
+
+    }
+
+    public void printListBackwards() {
+
+    }
+
+    private static class Node {
+
+        private Destination destination;
+        private Node next;
+        private Node previous;
+
+        public Node(Destination destination) {
+            this.destination = destination;
+            next = null;
+            previous = null;
+        }
+    }
+}
